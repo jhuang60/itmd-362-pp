ba0b619b5174d5425da5064f22224bb62e7c5ec3
Jiayu Huang <jhuang60@hawk.iit.edu>
2018-01-28 22:02:31 -0600

    variable elements added

---
+++ pp-01/site.js
@@ -0,0 +1,11 @@
+
+  $('#inputform').on('submit', function(e){
+    var email = $('#email').val();
+    var phone = $('#phone').val();
+    var birthday = $('#birthday').val();
+    var status = document.getElementById("status");
+    var fill = "Please fill in all the fields";
+    var done = "Form submitted";
+
+    e.preventDefault();
+  });
