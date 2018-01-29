ddce6568d4132c3d6a0698e01cf292741736aee4
Jiayu Huang <jhuang60@hawk.iit.edu>
2018-01-28 22:03:58 -0600

    check if form was all filled in

--- pp-01/site.js
+++ pp-01/site.js
@@ -8,4 +8,15 @@
     var done = "Form submitted";
 
     e.preventDefault();
+
+      if (email === '' || phone === '' || birthday === '') {
+        status.textContent = fill;
+        document.getElementById("status").style.color = 'red';
+
+        } else{
+          console.log("form submitted", email, phone, birthday);
+          status.textContent = done;
+          document.getElementById("status").style.color = 'green';
+        }
+
   });
