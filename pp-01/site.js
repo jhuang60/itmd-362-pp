a54fda044d33079ce0b0ab270bd5c25d3e8b9d73
Jiayu Huang <jhuang60@hawk.iit.edu>
2018-01-28 23:55:33 -0600

    phone validation

--- pp-01/site.js
+++ pp-01/site.js
@@ -23,6 +23,16 @@ $('#inputform').on('submit', function(e){
       $('#emaillistitem').append('<li id="removeemail">Please verify your email input</li>');
       correctinput = false;
     }
+    if (phone.match(/^[0-9]{11}$/)){
+      $('#removephone').remove();
+      $('#phonelistitem').append('<li id="removephone">correct phone input. </li>');
+      correctinput = true;
+    }
+    else{
+      $('#removephone').remove();
+      $('#phonelistitem').append('<li id="removephone">Please verify that your phone number input has 10 characters</li>');
+      correctinput = false;
+    }
     if( correctinput === true){
       console.log("form submitted", email, phone, birthday);
       status.textContent = done;
