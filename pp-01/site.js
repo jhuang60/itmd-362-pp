8d1bf52c3142d2d57de6a4c2c5cfdcb741bccd73
Jiayu Huang <jhuang60@hawk.iit.edu>
2018-01-28 23:55:07 -0600

    email validation

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,22 +1,34 @@
+$('#inputform').on('submit', function(e){
+  e.preventDefault();
+  var email = $('#email').val();
+  var phone = $('#phone').val();
+  var birthday = $('#birthday').val();
+  var status = document.getElementById("status");
+  var fill = "Please fill in all the fields";
+  var done = "Form submitted";
+  var correctinput = false;
 
-  $('#inputform').on('submit', function(e){
-    var email = $('#email').val();
-    var phone = $('#phone').val();
-    var birthday = $('#birthday').val();
-    var status = document.getElementById("status");
-    var fill = "Please fill in all the fields";
-    var done = "Form submitted";
+  if (email === '' || phone === '' || birthday === '') {
+    status.textContent = fill;
+    document.getElementById("status").style.color = 'red';
+  }
+  else {
+    if(email.match(/.+@.+/)){
+      $('#removeemail').remove();
+      $('#emaillistitem').append('<li id="removeemail">correct email input</li>');
+      correctinput = true;
+    }
+    else {
+      $('#removeemail').remove();
+      $('#emaillistitem').append('<li id="removeemail">Please verify your email input</li>');
+      correctinput = false;
+    }
+    if( correctinput === true){
+      console.log("form submitted", email, phone, birthday);
+      status.textContent = done;
+      document.getElementById("status").style.color = 'green';
+    }
 
-    e.preventDefault();
+  }
 
-      if (email === '' || phone === '' || birthday === '') {
-        status.textContent = fill;
-        document.getElementById("status").style.color = 'red';
-
-        } else{
-          console.log("form submitted", email, phone, birthday);
-          status.textContent = done;
-          document.getElementById("status").style.color = 'green';
-        }
-
-  });
+});
